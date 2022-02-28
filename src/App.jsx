9b80594404ba17ee7f4dfbcea1858a535e4fdd61
Antony-LE <le.antony.l@gmail.com>
2022-02-28 15:18:39 +0100

    add video in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Registration from './components/registration/Registration';
-import Login from './components/login/Login';
+import Login from './Components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Registration />
       <Login />
     </div>
   );
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-no-useless-fragment */
-import React, { useState, useEffect } from 'react';
 
+import React, { useState, useEffect } from 'react';
 import './login.css';
 
 // Import of npm icons and font from fontawesome.com
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import runningVideo from '../../Assets/running.webm';
 
 // Regex for the email validation ( exmaple@wanadoo.com format)
 const EMAIL_REGEX = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -21,8 +23,6 @@ function Login() {
   // handle states related to paswword
   const [pwd, setPwd] = useState('');
   const [validPwd, setValidPwd] = useState(false);
-  // handle wether the user has succefully logged in or not
-  const [success, setSuccess] = useState(false);
   // handle error
   const [errMsg, setErrMsg] = useState(false);
 
@@ -49,69 +49,73 @@ function Login() {
   // Handle the form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setSuccess(true);
   };
 
   return (
-    <>
-      {success ? (
-        <section className="runningWild__login">
-          <h1>Bienvenue sur Running Wild !</h1>
-          <p>
-            <a href="#">Se connecter</a>
+    <div className="runningwild__login-container">
+      <section className="runningwild__login-left">
+        <p className={!errMsg ? 'errmsg' : 'offscreen'}>{errMsg}</p>
+        <h1>Running Wild - Connexion</h1>
+        <form className="runningwild__login-form" onSubmit={handleSubmit}>
+          {/* ********************************email input******************************* */}
+          <label
+            className="runningwild__login-form-label"
+            htmlFor="email"
+          >
+            Votre Email:
+          </label>
+          <input
+            type="text"
+            className="runningwild__login-form-input"
+            id="email"
+            autoComplete="off"
+            onChange={(e) => setUserEmail(e.target.value)}
+            value={userEmail}
+            required
+          />
+          <p id="uidnote" className={userEmail && !validEmail ? 'instructions' : 'offscreen'}>
+            <FontAwesomeIcon icon={faInfoCircle} />
+            Format d&apos;email invalide
           </p>
-        </section>
-      ) : (
-        <section className="runningWild__login">
-          <p className={!errMsg ? 'errmsg' : 'offscreen'}>{errMsg}</p>
-          <h1>Running Wild - Connexion</h1>
-          <form className="runningWild__login-form" onSubmit={handleSubmit}>
-            {/* ********************************email input******************************* */}
-            <label htmlFor="email">
-              Votre Email:
-            </label>
-            <input
-              type="text"
-              id="email"
-              autoComplete="off"
-              onChange={(e) => setUserEmail(e.target.value)}
-              value={userEmail}
-              required
-            />
-            <p id="uidnote" className={userEmail && !validEmail ? 'instructions' : 'offscreen'}>
-              <FontAwesomeIcon icon={faInfoCircle} />
-              Format d&apos;email invalide
-            </p>
-            {/* ********************************password input******************************* */}
-            <label htmlFor="password">
-              Mot de passe:
-            </label>
-            <input
-              type="password"
-              id="password"
-              onChange={(e) => setPwd(e.target.value)}
-              value={pwd}
-              required
-            />
-            {/* ********************************Sign in button******************************* */}
-            <button
-              type="submit"
-              disabled={!!(!validEmail || !validPwd)}
-            >
-              Connexion
-            </button>
-          </form>
-          <p>
-            Mot de passe oublié ?
-            <br />
-            <span className="line">
-              {/* put router link here */}
-              <a href="#">Recevoir mes informations de connexion</a>
-            </span>
-          </p>
-        </section>
-      )}
-    </>
+          {/* ********************************password input******************************* */}
+          <label
+            className="runningwild__login-form-label"
+            htmlFor="password"
+          >
+            Mot de passe:
+          </label>
+          <input
+            type="password"
+            className="runningwild__login-form-input"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+          {/* ********************************Sign in button******************************* */}
+          <button
+            className="runningwild__login-form-buton"
+            type="submit"
+            disabled={!!(!validEmail || !validPwd)}
+          >
+            Connexion
+          </button>
+        </form>
+        <p>
+          Mot de passe oublié ?
+          <br />
+          <span className="line">
+            {/* put router link here */}
+            <a href="#">Recevoir mes informations de connexion</a>
+          </span>
+        </p>
+      </section>
+      <section className="runningwild__login-right">
+        <video alt="video of a runner in a field" autoPlay controls loop>
+          <source src={runningVideo} type="video/webm" />
+        </video>
+      </section>
+    </div>
   );
 }
 
