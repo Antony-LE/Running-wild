c8eb64a1f84cc8b057a702f99c25c8924172dbc7
Antony-LE <le.antony.l@gmail.com>
2022-03-01 14:59:01 +0100

    add fix on components folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Registration from './Components/registration/Registration';
+import Registration from './components/registration/Registration';
 
 function App() {
   return (
