96993dada5dcc04e1b2ea36c175819a47f8b85d6
Antony-LE <le.antony.l@gmail.com>
2022-03-06 21:02:57 +0100

    add Css style

--- src/Components/containers/profileCard/ProfileCard.jsx
+++ src/Components/containers/profileCard/ProfileCard.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import './profileCard.css';
+import localPoint from '../../../Assets/localpoint.png';
 
 // Import of axios
 import axios from '../../../api/axios';
@@ -12,45 +13,53 @@ to dynamically get the user's datas */
 const USER_ID_URL = `/user/${localStorage.getItem('id')}`;
 
 function ProfileCard({
-  className, name, role, rank, dateOfBirth, city, pseudo, about,
+  className, name, surname, role, rank, dateOfBirth, city, pseudo, about, avatar,
 }) {
   const handleLocalStorage = async (e) => {
     const response = await axios.get(USER_ID_URL);
     // Stored all the datas relative to the user into the localStorage
-    // localStorage.setItem('name', JSON.stringify(response.data.user_name));
-    // localStorage.setItem('rank', JSON.stringify(response.data.user_rank_id));
-    // localStorage.setItem('date Of Birth', JSON.stringify(response.data.user.dob));
-    // localStorage.setItem('city', JSON.stringify(response.data.user.city));
-    // localStorage.setItem('pseudo', JSON.stringify(response.data.user.pseudo));
-    // localStorage.setItem('about', JSON.stringify(response.data.user.about));
-    // localStorage.setItem('role', JSON.stringify(response.data.user.role_id));
-    // console.log(response);
+    localStorage.setItem('name', JSON.stringify(response.data.user.name));
+    localStorage.setItem('rank', JSON.stringify(response.data.user.rank_id));
+    localStorage.setItem('date Of Birth', JSON.stringify(response.data.user.dob));
+    localStorage.setItem('city', JSON.stringify(response.data.user.city));
+    localStorage.setItem('pseudo', JSON.stringify(response.data.user.pseudo));
+    localStorage.setItem('about', JSON.stringify(response.data.user.about));
+    localStorage.setItem('role', JSON.stringify(response.data.user.role_id));
+    localStorage.setItem('avatar', JSON.stringify(response.data.user.avatar));
+    localStorage.setItem('surname', JSON.stringify(response.data.user.surname));
+    console.log(response);
   };
   handleLocalStorage();
   return (
     <div className="runningwild_profileCard">
       <div className="runningwild_profileCard-upperContainer">
         <div className="runningwild_profileCard-imageContainer">
-          <img src="https://www.infomoney.com.br/wp-content/uploads/2019/06/homer-simpson.jpg?fit=900%2C734&quality=75&strip=all" alt="" height="100px" width="100px" />
+          <img src={avatar} alt="" height="100px" width="100px" />
         </div>
       </div>
       <div className="runningwild_profileCard-lowerContainer">
-        <h3>
-          {name}
+        <h2>
+          {name.replaceAll('"', '')}
+          {' '}
+          {surname.replaceAll('"', '')}
           <br />
-          Your rank :
+        </h2>
+        <h2>
+          Rank :
           {rank}
-          <br />
-          {dateOfBirth}
-          <br />
-          {city}
-          <br />
+        </h2>
+        <h3>
+          <img src={localPoint} width="20px" height="20px" alt="map point" />
+          {city.replaceAll('"', '')}
         </h3>
+        <br />
         <h4>
-          {pseudo}
+          Votre Pseudo :
+          {' '}
+          {pseudo.replaceAll('"', '')}
         </h4>
-        <p>{about}</p>
-        <button type="button" className="runningwild-profile-card-lower-container-button">Détails...</button>
+        <hr />
+        <button type="button" className="runningwild-profile-card-lower-container-button">Plus de détails</button>
       </div>
     </div>
   );
--- src/Components/containers/profileCardList/ProfileCardList.jsx
+++ src/Components/containers/profileCardList/ProfileCardList.jsx
@@ -1,36 +1,20 @@
+/* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import './profileCardList.css';
 import ProfileCard from '../profileCard/ProfileCard';
 
 function ProfileCardList() {
-  const [name, setName] = useState('Your Name');
-  const [rank, setRank] = useState(1);
-  const [role, setRole] = useState(1);
-  const [pseudo, setPseudo] = useState('Your Pseudo');
-  const [dateOfBirth, setDateOfBirth] = useState('Your DOB');
-  const [city, setCity] = useState('Your City');
-  const [about, setAbout] = useState('Lorem ipsum dolor sit amet. Et perspiciatis molestias et autem ratione sed velit explicabo. Eos voluptates dolor 33 velit doloremque.');
-
-  // Retrieve data from the localstorage and sent by profileCard page.
-  // setName(localStorage.getItem('name'));
-  // setRank(localStorage.getItem('rank'));
-  // setRole(localStorage.getItem('role'));
-  // setPseudo(localStorage.getItem('pseudo'));
-  // setDateOfBirth(localStorage.getItem('date Of Birth'));
-  // setCity(localStorage.getItem('city'));
-  // setRole(localStorage.getItem('role'));
-  // setAbout(localStorage.getItem('about'));
-
   return (
     <div className="profileCardList">
       <ProfileCard
-        name={name}
-        rank={rank}
-        pseudo={pseudo}
-        dateOfBirth={dateOfBirth}
-        city={city}
-        about={about}
+        name={localStorage.getItem('name')}
+        rank={localStorage.getItem('rank')}
+        pseudo={localStorage.getItem('pseudo')}
+        dateOfBirth={localStorage.getItem('date Of Birth')}
+        city={localStorage.getItem('city')}
+        about={localStorage.getItem('about')}
+        surname={localStorage.getItem('surname')}
       />
     </div>
   );
