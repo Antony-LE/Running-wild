8f74a6b7e94142327f7133ac0f1c5089afeb81b7
Frederic Klichowski <frederic.klichowski@gmail.com>
2022-03-08 13:33:14 +0100

    Challenges carroussel

--- src/Components/containers/challengesCardList/ChallengesCardList.jsx
+++ src/Components/containers/challengesCardList/ChallengesCardList.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './challengesCardList.css';
 import ChallengesCard from '../challengesCard/ChallengesCard';
 import axios from '../../../api/axios';
 
 const CHALLENGE_URL = '/challenge/all';
 
-function ChallengesCardList({ className }) {
+function ChallengesCardList() {
   const [challenges, setChallenges] = useState([]);
 
   const getChallenges = async () => {
@@ -18,37 +18,35 @@ function ChallengesCardList({ className }) {
   useEffect(() => getChallenges(), []);
 
   return (
-    <div className={className}>
+    <ul className="runningwild__challenges-content-cardList">
       {challenges.map((challenge) => (
-        <ChallengesCard
-          title={challenge.name}
-          key={challenge.challenge_id}
-          illustration={challenge.challenge_image}
-          text={challenge.description}
-          currentValue={challenge.distance}
-          maxValue={challenge.distance}
-        />
+        <li>
+          <ChallengesCard
+            title={challenge.name}
+            key={challenge.challenge_id}
+            illustration={challenge.challenge_image}
+            text={challenge.description}
+            currentValue={challenge.distance}
+            maxValue={challenge.distance}
+          />
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
-ChallengesCardList.propTypes = {
-  className: PropTypes.string,
-  // cardChallengesData: PropTypes.arrayOf(
-  //   PropTypes.shape({
-  //     title: PropTypes.string.isRequired,
-  //     text: PropTypes.string.isRequired,
-  //     illustration: PropTypes.string.isRequired,
-  //     currentValue: PropTypes.number.isRequired,
-  //     maxValue: PropTypes.number.isRequired,
-  //     id: PropTypes.number.isRequired,
-  //   }).isRequired,
-  // ).isRequired,
-};
-
-ChallengesCardList.defaultProps = {
-  className: 'runningwild__homepage-content-cardList',
-};
+// ChallengesCardList.propTypes = {
+//   className: PropTypes.string,
+//   cardChallengesData: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       title: PropTypes.string.isRequired,
+//       text: PropTypes.string.isRequired,
+//       illustration: PropTypes.string.isRequired,
+//       currentValue: PropTypes.number.isRequired,
+//       maxValue: PropTypes.number.isRequired,
+//       id: PropTypes.number.isRequired,
+//     }).isRequired,
+//   ).isRequired,
+// };
 
 export default ChallengesCardList;
