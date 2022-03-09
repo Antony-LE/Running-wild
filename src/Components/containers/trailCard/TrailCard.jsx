85360b6a02f4862bccd4f294f01ee5cf4d7f95e7
Antony-LE <le.antony.l@gmail.com>
2022-03-09 17:40:06 +0100

    Add all axios requests on trail page

--- src/Components/containers/trailCard/TrailCard.jsx
+++ src/Components/containers/trailCard/TrailCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/control-has-associated-label */
@@ -5,7 +6,9 @@
 import React, { useState } from 'react';
 import './trailCard.css';
 
-function TrailCard() {
+function TrailCard({
+  name, city, map, environement, distance, startPoint, endPoint, postalCode, like,
+}) {
   const [detailsOn, setDetailsOn] = useState(false);
 
   const handleDisplayDetails = () => {
@@ -13,16 +16,60 @@ function TrailCard() {
   };
   return (
     <div className="runningwild_trailCard">
-      <div className="runningwild_trailCard-upperContainer" />
+      <div className="runningwild_trailCard-upperContainer">
+        {name}
+      </div>
+      <div className="runningwild_trailCard-imageContainer">
+        <iframe src={map} />
+      </div>
       <div className="runningwild_trailCard-lowerContainer">
+        <span>
+          Type d&apos;environnement :
+          {' '}
+          {environement}
+          {' '}
+        </span>
+        <span>
+          Distance :
+          {' '}
+          {`${distance}km`}
+          {' '}
+        </span>
+        <span>
+          Ville :
+          {' '}
+          {city}
+          {' '}
+        </span>
+        <span>
+          Point de départ :
+          {' '}
+          {startPoint}
+          {' '}
+        </span>
 
         {detailsOn
           ? (
             <>
-              <span />
-              <span />
-              <span />
-              <span />
+              <hr />
+              <span>
+                Point d&apos;arrivée :
+                {' '}
+                {endPoint}
+                {' '}
+              </span>
+              <span>
+                Code postal :
+                {' '}
+                {postalCode}
+                {' '}
+              </span>
+              <span>
+                Nombre de likes :
+                {' '}
+                {`${like}❤️`}
+                {' '}
+              </span>
             </>
           )
           : ''}
--- src/Components/containers/trailCardList/TrailCardList.jsx
+++ src/Components/containers/trailCardList/TrailCardList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 /* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
@@ -7,17 +8,65 @@ import TrailCard from '../trailCard/TrailCard';
 import Navbar from '../../navbar/Navbar';
 import Footer from '../../footer/Footer';
 
+// Import of axios
+import axios from '../../../api/axios';
+
 // logged in variable
 const isLoggedIn = localStorage.getItem('id');
 
+/* Use the variable id from localStorage (previsously stored in the login page)
+to dynamically get the user's datas */
+const TRAIL_ID_URL = `/trail/${17}`;
+
 function TrailCardList() {
+  const [name, setName] = useState('');
+  const [city, setCity] = useState('');
+  const [map, setMap] = useState('');
+  const [environement, setEnvironement] = useState([]);
+  const [distance, setDistance] = useState('');
+  const [startpoint, setStartPoint] = useState('');
+  const [endpoint, setEndpoint] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [like, setLike] = useState(0);
+
+  const handleTrailData = async () => {
+    const response = await axios.get(TRAIL_ID_URL);
+    setName(response.data.trail.name);
+    setCity(response.data.trail.city);
+    setMap(response.data.trail.route);
+    if (response.data.trail.categoryid[0] === 1) {
+      setEnvironement('Urbain');
+    } else if (response.data.trail.categoryid[0] === 2) {
+      setEnvironement('Forêt');
+    } else if (response.data.trail.categoryid[0] === 3) {
+      setEnvironement('Mer');
+    } else if (response.data.trail.categoryid[0] === 4) {
+      setEnvironement('Montagne');
+    }
+    setDistance(response.data.trail.distance);
+    setStartPoint(response.data.trail.start_point);
+    setEndpoint(response.data.trail.end_point);
+    setPostCode(response.data.trail.postcode);
+    setLike(response.data.trail.like.like);
+  };
+  handleTrailData();
   return (
     <>
       {isLoggedIn ? (
         <>
           <Navbar />
           <div className="trail-card-list">
-            <TrailCard />
+            <TrailCard name={name} city={city} map={map} environement={environement} distance={distance} startPoint={startpoint} endPoint={endpoint} postalCode={postCode} like={like} />
+          </div>
+          <div className="recherche">
+            <hr />
+            <h2> Rechercher un itinéraire par : </h2>
+            <button type="button" className="runningwild-trail-card-button">
+              Autour de vous
+            </button>
+            <button type="button" className="runningwild-trail-card-button">
+              Selon la distance
+            </button>
           </div>
           <Footer />
         </>
