07952f82594be49ee6b112bc972925fae81b7596
Antony-LE <le.antony.l@gmail.com>
2022-03-10 19:16:01 +0100

    Add subscription button on search results

--- src/Components/containers/trailCardList/TrailCardList.jsx
+++ src/Components/containers/trailCardList/TrailCardList.jsx
@@ -28,6 +28,13 @@ localStorage.setItem('trail_Id', randomValue);
 // endpoint for random trail
 const TRAIL_ID_URL = `/trail/${randomValue}`;
 
+// endpoint to subscribe to a new run
+const RUN_URL = '/run';
+
+// variables to get the current time
+const t = new Date();
+const hours = `${t.getHours()}:${t.getMinutes()}`;
+
 function TrailCardList() {
   const [name, setName] = useState('');
   const [city, setCity] = useState('');
@@ -40,6 +47,15 @@ function TrailCardList() {
   const [endpoint, setEndpoint] = useState('');
   const [postCode, setPostCode] = useState('');
   const [like, setLike] = useState(0);
+
+  // Handle the SUBSCRIPTION BUTTON
+  // display the button subscription style
+  const [subscriptionOn, setSubscriptionOn] = useState(false);
+  const [userId, setUserId] = useState(0);
+  const [trailId, setTrailId] = useState(0);
+  const [time, setTime] = useState('');
+  const [hour, setHour] = useState('');
+  const [Km, setKm] = useState(0);
 
   // Handle datas for search form
   const [searchedCity, setSearchedCity] = useState('');
@@ -83,124 +99,159 @@ function TrailCardList() {
     console.log(searchedResults);
   };
 
+  const handleSubscription = async (e) => {
+    setUserId(localStorage.getItem('id'));
+    setTrailId(localStorage.getItem('trail_Id'));
+    setTime('01:30:50');
+    setHour(hours);
+    setKm(localStorage.getItem('distance'));
+    setSubscriptionOn(!subscriptionOn);
+    try {
+      const response = await axios.post(
+        RUN_URL,
+        JSON.stringify({
+          userId,
+          trailId,
+          time,
+          hour,
+          distance: Km,
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+
+        },
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       {isLoggedIn ? (
         <>
-          <Navbar />
-          <h1>Suggestion de parcours</h1>
-          <div className="reload">
-            {' '}
-            <button type="button" onClick={handleReload}>
-              Suggestion suivante
-            </button>
-            {' '}
-          </div>
-          <div className="trail-card-list">
-            <TrailCard name={name} city={city} map={map} environement={environement} distance={distance} startPoint={startpoint} endPoint={endpoint} postalCode={postCode} like={like} />
-          </div>
-          <div className="runningwild-search-form">
-            <hr />
-            <h2> Rechercher un itinéraire par : </h2>
-            <form>
-              {/* ********************************City input******************************* */}
-              <label
-                className="runningwild__login-form-label"
-                htmlFor="city"
-              />
-              <input
-                type="text"
-                placeholder="Ville"
-                className="runningwild__login-form-input"
-                id="city"
-                autoComplete="off"
-                onChange={(e) => setSearchedCity(e.target.value)}
-                value={searchedCity}
-                required
-              />
-              {/* ********************************MinDistance input******************************* */}
-              <label
-                className="runningwild__login-form-label"
-                htmlFor="min-distance"
-              />
-              <input
-                type="text"
-                placeholder="Distance Min.(KM)"
-                className="runningwild__login-form-input"
-                id="min-distance"
-                onChange={(e) => setDistanceMin(e.target.value)}
-                value={distanceMin}
-                required
-              />
-            </form>
-            <button
-              onClick={handleSearchData}
-              className="runningwild__search-form-button"
-              type="submit"
-            >
-              Rechercher !
-            </button>
-            <hr />
-            {displayResults ? (
-              <div className="search-results">
-                <h2> Résultats de votre recherche : </h2>
-                { searchedResults.map((searchResult) => (
-                  <>
-                    <span>
-                      Nom :
-                      {' '}
-                      {searchResult.name}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      Environnement :
-                      {' '}
-                      {searchResult.categoryid}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      Distance :
-                      {' '}
-                      {searchResult.distance}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      ville :
-                      {' '}
-                      {searchResult.city}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      Adresse de départ :
-                      {' '}
-                      {searchResult.start_point}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      Adresse d&apos;arrivée :
-                      {' '}
-                      {searchResult.endpoint}
-                      {' '}
-                    </span>
-                    <br />
-                    <span>
-                      Code Postal :
-                      {' '}
-                      {searchResult.postcode}
-                      {' '}
-                    </span>
-                    <br />
-                    <span />
-                    <hr />
-                  </>
-                ))}
-              </div>
-            ) : ('')}
+          <div className="trail-card-list-container">
+            <Navbar />
+            <h1>Suggestion de parcours</h1>
+            <div className="reload">
+              {' '}
+              <button type="button" onClick={handleReload}>
+                Suggestion suivante
+              </button>
+              {' '}
+            </div>
+            <div className="trail-card-list">
+              <TrailCard name={name} city={city} map={map} environement={environement} distance={distance} startPoint={startpoint} endPoint={endpoint} postalCode={postCode} like={like} />
+            </div>
+            <div className="runningwild-search-form">
+              <hr />
+              <h2> Rechercher un itinéraire par : </h2>
+              <form>
+                {/* ********************************City input******************************* */}
+                <label
+                  className="runningwild__login-form-label"
+                  htmlFor="city"
+                />
+                <input
+                  type="text"
+                  placeholder="Ville"
+                  className="runningwild__login-form-input"
+                  id="city"
+                  autoComplete="off"
+                  onChange={(e) => setSearchedCity(e.target.value)}
+                  value={searchedCity}
+                  required
+                />
+                {/* ********************************MinDistance input******************************* */}
+                <label
+                  className="runningwild__login-form-label"
+                  htmlFor="min-distance"
+                />
+                <input
+                  type="text"
+                  placeholder="Distance Min.(KM)"
+                  className="runningwild__login-form-input"
+                  id="min-distance"
+                  onChange={(e) => setDistanceMin(e.target.value)}
+                  value={distanceMin}
+                  required
+                />
+              </form>
+              <button
+                onClick={handleSearchData}
+                className="runningwild__search-form-button"
+                type="submit"
+              >
+                Rechercher !
+              </button>
+              <hr />
+              {displayResults ? (
+                <div className="search-results">
+                  <h2> Résultats de votre recherche : </h2>
+                  { searchedResults.map((searchResult) => (
+                    <>
+                      <span>
+                        Nom :
+                        {' '}
+                        {searchResult.name}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        Environnement :
+                        {' '}
+                        {searchResult.categoryid}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        Distance :
+                        {' '}
+                        {searchResult.distance}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        ville :
+                        {' '}
+                        {searchResult.city}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        Adresse de départ :
+                        {' '}
+                        {searchResult.start_point}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        Adresse d&apos;arrivée :
+                        {' '}
+                        {searchResult.end_point}
+                        {' '}
+                      </span>
+                      <br />
+                      <span>
+                        Code Postal :
+                        {' '}
+                        {searchResult.postcode}
+                        {' '}
+                      </span>
+                      <br />
+                      <span />
+                      <button type="button" className="runningwild-trail-card-list-subscribtion" onClick={handleSubscription}>
+                        {subscriptionOn === true ? (<>Inscris !</>) : (<>Je m&apos;inscris</>)}
+                      </button>
+                      <hr />
+                    </>
+                  ))}
+                </div>
+              ) : ('')}
+            </div>
           </div>
           <Footer />
         </>
